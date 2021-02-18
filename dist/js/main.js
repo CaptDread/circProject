761e5765134a60035f163d808abfcdf39e5d95e0
CaptDread <domlycaon@gmail.com>
2021-02-17 19:10:39 -0500

    circFin"

---
+++ dist/js/main.js
@@ -0,0 +1,30 @@
+"use strict";
+
+console.log("Hello World from main.js! \nChange this message, and make sure it changes in the browser \nto verify that you're working in the right files.");
+var burgerButt = document.querySelector(".daburger");
+var topBun = document.querySelector(".topbun");
+var mid = document.querySelector(".dagoods");
+var bottBun = document.querySelector(".botbun");
+var burgerBar = document.querySelector(".burgerMenu");
+var burgNav = document.querySelector(".burgNav"); // var burgerBlur = document.querySelector(`.blur`)
+
+var toggleBurger = function toggleBurger() {
+  if (burgerButt.id != "open") {
+    burgerButt.id = "open";
+    burgerBar.style.display = "block";
+    burgNav.style.display = "block"; // burgerBlur.style.display = 'block';
+    // burgerBlur.style.opacity = "90%";
+  } else if (burgerButt.id != "close") {
+    burgerButt.id = "close";
+    burgerBar.style.display = "none";
+    burgNav.style.display = "none"; // burgerBlur.style.display = 'none';
+  }
+
+  console.log("burgerMenu is now ", burgerButt.id);
+};
+
+burgerButt.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  toggleBurger();
+});
+//# sourceMappingURL=main.js.map
